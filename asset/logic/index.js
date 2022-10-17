94b0ea69f44286a23b0a4e87743d6dac1e3505ac
NebiatH Nana <nebiathn@gmail.com>
2022-10-17 14:58:59 -0400

    adding function to register task

--- asset/logic/index.js
+++ asset/logic/index.js
@@ -17,3 +17,36 @@ var excuteTask = function () {
   var textFifteen = localStorage.getItem(15);
   var textSixteen = localStorage.getItem(16);
   var textSeventeen = localStorage.getItem(17);
+  
+  $("#9").val(texNine);
+  $("#10").val(textTen);
+  $("#11").val(textEleven);
+  $("#12").val(textTwelve);
+  $("#13").val(textThirteen);
+  $("#14").val(textFourteen);
+  $("#15").val(textFifteen);
+  $("#16").val(textSixteen);
+  $("#17").val(textSeventeen);
+
+  $(".detail").each(function (i) {
+    var hour = i + 9;
+
+    if (hour < currentHour) {
+      $(this).addClass("past");
+    }
+    if (hour == currentHour) {
+      $(this).addClass("present");
+    }
+    if (hour > currentHour) {
+      $(this).addClass("future");
+    }
+  });
+};
+
+$(".row").on("click", ".saveBtn", function () {
+  var tasktext = $(this).parent().find(".detail").val().trim();
+  var time = $(this).parent().find(".detail").attr("id");
+  localStorage.setItem(time, tasktext);
+});
+
+excuteTask();
